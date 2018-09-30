588402eb808ea9226d0b3a7f9cd53bac9162101c
James <43457025+Fortiam@users.noreply.github.com>
2018-09-30 15:51:55 -0400

    Add files via upload

---
+++ shouter.js
@@ -0,0 +1,28 @@
+function shouter(whatToShout) {
+  // your code here
+  let newStr = whatToShout.toUpperCase() + "!!!";
+  console.log(`newStr is ${newStr}`);
+  return newStr;
+}
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+// tests
+
+function testShouter() {
+  const whatToShout = 'fee figh foe fum';
+  const expected = 'FEE FIGH FOE FUM!!!';
+  if (shouter(whatToShout) === expected) {
+    console.log('SUCCESS: `shouter` is working');
+  } else {
+    console.log('FAILURE: `shouter` is not working');
+  }
+}
+
+testShouter();
---
+++ textnormalizer.js
@@ -0,0 +1,29 @@
+function textNormalizer(text) {
+  // your code here
+  let str = text.toLowerCase();  
+  str = str.replace(/^\s*/, "");  //regexp replace ^(at start) \s(spaces) *(any number), replace with ""  
+  str = str.replace(/\s*$/, "");  //regexp replace (\s)space *(any amount of) $(at end), to ""
+  return str;
+}
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+// tests
+
+function testTextNormalizer() {
+  const text = "   let's GO SURFING NOW everyone is learning how   ";
+  const expected = "let's go surfing now everyone is learning how";
+  if (textNormalizer(text) === expected) {
+    console.log('SUCCESS: `textNormalizer` is working');
+  } else {
+    console.log('FAILURE: `textNormalizer` is not working');
+  }
+}
+
+testTextNormalizer();
